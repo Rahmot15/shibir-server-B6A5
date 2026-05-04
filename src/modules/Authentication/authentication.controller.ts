6692336074b17cb13f312a302e6179caec1582fd
Rahmot15 <mdrahmatulla666@gmail.com>
2026-05-04 09:09:03 +0600

    feat: implement login functionality and response handler in authentication module

--- src/modules/Authentication/authentication.controller.ts
+++ src/modules/Authentication/authentication.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync.js";
 import { AuthService } from "./authentication.service.js";
+import { sendResponse } from "../../shared/sendResponse.js";
 
 const registerSupporter = catchAsync(
     async (req: Request, res: Response) => {
@@ -10,7 +11,7 @@ const registerSupporter = catchAsync(
 
         const result = await AuthService.RegisterSupporter(payload);
 
-        res.status(201).json({
+        sendResponse(res, {
             httpStatusCode: 201,
             success: true,
             message: "Supporter registered successfully",
@@ -19,6 +20,20 @@ const registerSupporter = catchAsync(
     }
 )
 
+const loginUser = catchAsync(
+    async (req: Request, res: Response) => {
+        const payload = req.body;
+        const result = await AuthService.loginUser(payload);
+        sendResponse(res, {
+            httpStatusCode: 200,
+            success: true,
+            message: "User logged in successfully",
+            data: result,
+        })
+    }
+)
+
 export const AuthController = {
     registerSupporter,
+    loginUser,
 };
--- src/modules/Authentication/authentication.service.ts
+++ src/modules/Authentication/authentication.service.ts
@@ -1,3 +1,4 @@
+import { UserStatus } from "@prisma/client";
 import { auth } from "../../lib/auth.js";
 
 interface IRegisterPatientPayload {
@@ -24,6 +25,34 @@ const RegisterSupporter = async (payload: IRegisterPatientPayload) => {
   return data;
 };
 
+interface ILoginUserPayload {
+    email: string;
+    password: string;
+}
+
+const loginUser = async (payload: ILoginUserPayload) => {
+    const { email, password } = payload;
+
+    const data = await auth.api.signInEmail({
+        body: {
+            email,
+            password,
+        }
+    })
+
+    if (data.user.status === UserStatus.BLOCKED) {
+        throw new Error("User is blocked");
+    }
+
+    if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+        throw new Error("User is deleted");
+    }
+
+    return data;
+
+}
+
 export const AuthService = {
   RegisterSupporter,
+    loginUser,
 };
